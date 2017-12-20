d6fd7e2c3c395ba800e551183a459e93b202b62f
Adam Jayne <adamjayne@Hallies-MacBook-Pro.local>
2017-12-20 16:19:30 -0500

    Finished day 2 part 2

--- dayTwoSolution.py
+++ dayTwoSolution.py
@@ -9,3 +9,11 @@ for row in resource.split("\n"):
 print(total)
 
 # Part 2
+total2 = 0
+for row in resource.split("\n"):
+    thisRow = list(map(int, row.split()))
+    for pos1 in range(0, len(thisRow)):
+        for pos2 in range(0, len(thisRow)):
+            if (thisRow[pos1] != thisRow[pos2]) & (thisRow[pos1] % thisRow[pos2] == 0):
+                total2 = total2 + int(thisRow[pos1] / thisRow[pos2])
+print(total2)
