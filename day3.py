65dd7a40ffc2a9ff822a00e926d06324bf464aec
Adam Jayne <adamjayne@Hallies-MacBook-Pro.local>
2017-12-21 11:24:13 -0500

    skipped day 3 and moved to finish day 4

---
+++ day3.py
@@ -0,0 +1,15 @@
+myFile = open("./resources/day3Resource")
+readFile = int(myFile.read())
+
+def layer(x):
+    odd = 1
+    while x < odd ** 2:
+        odd += 2
+    return odd
+
+
+def area(x):
+
+
+
+# DISCLAIMER, THIS IS NOT FINISHED
---
+++ day4.py
@@ -0,0 +1,40 @@
+resource = open("./resources/day4Resource")
+phrases = resource.read()
+
+# part 1
+
+v1 = 0
+for r in phrases.split('\n'):
+    c = 0
+    e = list(map(str, r.split()))
+    for i1 in range(0, len(e)):
+        for i2 in range(0, len(e)):
+            if (e[i1] == e[i2]) and (i1 != i2):
+                c += 1
+    if c == 0:
+        v1 += 1
+print(v1)
+
+# part 2
+
+def chkanagram(x, y):
+    l1 = list(x)
+    l2 = list(y)
+    l1.sort()
+    l2.sort()
+    return l1 == l2
+
+
+v2 = 0
+for r in phrases.split('\n'):
+    c = 0
+    e = list(map(str, r.split()))
+    for i1 in range(0, len(e)):
+        for i2 in range(0, len(e)):
+            if (i1 != i2) and (e[i1] == e[i2]):
+                c += 1
+            elif (i1 != i2) and chkanagram(e[i1], e[i2]):
+                c += 1
+    if c == 0:
+        v2 += 1
+print(v2)
