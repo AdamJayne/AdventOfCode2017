f789109abf22cebf8be4cd579993592eec8db2ea
Adam Jayne <adamjayne402@gmail.com>
2018-11-15 20:37:42 -0500

    Almost finished day7

---
+++ day7.py
@@ -0,0 +1,18 @@
+data = open("./resources/day7Resource").read()
+
+formatted = data.split("\n")
+shrinked = []
+topLevel = []
+names = []
+for i in formatted:
+    x = i.split(" -> ")
+    if len(x) == 2:
+        shrinked.append(x)
+for i in shrinked:
+    name = i[0].split()
+    names.append(name[0])
+    top = i[1].split(", ")
+    for v in top:
+        topLevel.append(v)
+for i in names:
+    if 
